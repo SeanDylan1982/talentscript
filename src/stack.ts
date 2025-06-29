d912cacad1f6a127bb210f521d09fffbc48470f4
Sean Dylan Patterson <seandylanpatterson@gmail.com>
2025-06-29 14:57:21 +0200

    Setup Stack Auth for Vite React Project

--- src/stack.ts
+++ src/stack.ts
@@ -1,7 +1,7 @@
 import { StackClientApp } from "@stackframe/stack";
 
 export const stackClientApp = new StackClientApp({
-  projectId: import.meta.env.VITE_STACK_PROJECT_ID,
-  publishableClientKey: import.meta.env.VITE_STACK_PUBLISHABLE_KEY,
+  projectId: import.meta.env.VITE_STACK_PROJECT_ID || "",
+  publishableClientKey: import.meta.env.VITE_STACK_PUBLISHABLE_KEY || "",
   tokenStore: "cookie",
 });
